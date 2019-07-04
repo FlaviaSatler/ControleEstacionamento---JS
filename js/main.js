9a8fbb33aa3e754af02333c13ef03f8552593bcf
Flavia <flaviasatler@hotmail.com>
2019-07-04 16:25:33 -0300

    add function checkout car

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 (function () {
     const $ = q => document.querySelector(q);
 
+    function convertPeriod(mil){
+        const min = Math.floor(mil / 60000);
+        const sec = Math.floor((mil % 60000) / 1000);
+
+        return ` ${min}m e ${sec}s;`
+
+    }
+
     function renderGarage() {
         const garage = getGarage();
+        $("#garage").innerHTML = '';
 
         garage.forEach(c => addCarToGarage(c));
     }
@@ -12,7 +21,7 @@
         row.innerHTML = `
             <td>${car.name}</td>
             <td>${car.licence}</td>
-            <td>${new Date(car.time)
+            <td data-time="${car.time}">${new Date(car.time)
                 .toLocaleString("pt-BR", {
                 hour: "numeric",
                 minute: "numeric"
@@ -24,6 +33,23 @@
 
         garage.appendChild(row);
     };
+
+    function checkOut(info){
+       let period = new Date() - new Date(info[2].dataset.time); 
+       period = convertPeriod(period);
+       
+       const licence = info[1].textContent;
+       const msg = `O veículo ${info[1].textContent} de placa ${licence} permaneceu estacionado por ${period}
+       Deseja encerrar?`;
+
+       if(!confirm(msg)) return;
+        
+       const garage = getGarage().filter(c => c.licence !== licence);
+       localStorage.garage = JSON.stringify(garage);
+
+       renderGarage();
+
+    }
 
     const getGarage = () => localStorage.garage ? JSON.parse (localStorage.garage) :[];
 
@@ -48,5 +74,11 @@
 
         $('#name').value = "";
         $('#licence').value = "";
+    })
+
+    $('#garage').addEventListener("click", function (e){
+        if(e.target.className == "delete"){
+            checkOut(e.target.parentElement.parentElement.cells);
+        }
     })
 })()
