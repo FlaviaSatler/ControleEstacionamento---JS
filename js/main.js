b6eea53337e50c58ef4b8754e64218aa9ab408f4
Flavia <flaviasatler@hotmail.com>
2019-07-04 11:59:16 -0300

    Add initial html structure and main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function () {
+    const $ = q => document.querySelector(q);
+
+    const register = $('#send');
+    register.addEventListener("click", function(){
+        const name = $('#name');
+        const licence = $('#licence');
+
+        alert("Aeee")
+    })
+})()
