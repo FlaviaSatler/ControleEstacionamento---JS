c980923108bc26a16bffe111e37e38efbd346acd
Flavia <flaviasatler@hotmail.com>
2019-07-04 12:28:07 -0300

    creating a new append - table elements

--- js/main.js
+++ js/main.js
@@ -1,11 +1,40 @@
 (function () {
     const $ = q => document.querySelector(q);
 
+    function addCarToGarage (car) {
+        const row= document.createElement("tr");
+        row.innerHTML = `
+            <td>${car.name}</td>
+            <td>${car.licence}</td>
+            <td>${car.time}</td>
+            <td>
+                <button class="delete">X</button>
+            </td>
+        `;
+
+        garage.appendChild(row);
+    }
+
     const register = $('#send');
     register.addEventListener("click", function(){
-        const name = $('#name');
-        const licence = $('#licence');
+        const name = $('#name').value;
+        const licence = $('#licence').value;
 
-        alert("Aeee")
+        if (!name || !licence) {
+            alert('Os campos são obrigatórios !')
+            return;
+        }
+        
+        const car = {name, licence, time: new Date()}
+
+        const garage = localStorage.garage ? JSON.parse (localStorage.garage) : [];
+        garage.push(car);
+        localStorage.garage = JSON.stringify(garage)
+        console.log (garage)
+
+        addCarToGarage(car);
+
+        $('#name').value = "";
+        $('#licence').value = "";
     })
 })()
